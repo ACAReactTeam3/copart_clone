ba5ef2a01a3ad526070aaef5144c7c62ac04b1be
Ovsanna <ovsannaovsanna16@gmail.com>
2023-01-26 01:11:31 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,69 +1,86 @@
 import "./App.css";
 import React from "react";
-import SignIn from './components/registration/SignIn'
-import SignUp from './components/registration/SignUp'
-import Home from './components/Home'
-import { useDispatch, useSelector } from 'react-redux'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
-import CreatePost from './components/CreatePost'
-
-import { getDatabase, ref, set} from "firebase/database";
-import User from './components/User'
-
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import SignIn from "./components/registration/SignIn";
+import SignUp from "./components/registration/SignUp";
+import Home from "./components/Home";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { getDatabase, ref, set } from "firebase/database";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "./firebase/firebase";
 import Nav from "./components/nav/Nav";
-import Account from "./components/account/Account";
 
-function App() {    
+function App() {
   let db = getDatabase();
-  let navigate = useNavigate()
-  let dispatch = useDispatch()
- 
-  let user = useSelector(function(store) {
-    return store.map((item => {return item}))
-  })
-  const signIn = ( email, password) => {
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        navigate("/")
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-    });
-}
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
 
-  const signUp = async (email, password, name, surname) => {   
-    await createUserWithEmailAndPassword(auth, email, password)
+  let user = useSelector(function (store) {
+    return store.map((item) => {
+      return item;
+    });
+  });
+  const signIn = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-          const user = userCredential.user;
-          db = set(ref(db, 'users/ ' + user.uid), {
-            name: name,
-            surname: surname,
-            posts: [],
-            saved: []
-          });
+        const user = userCredential.user;
+        navigate("/");
       })
       .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorCode, errorMessage);
-      }); 
-  }
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  const signUp = async (email, password, name, surname) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        db = set(ref(db, "users/ " + user.uid), {
+          name: name,
+          surname: surname,
+          posts: [],
+          saved: [],
+        });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
   const logout = async () => {
-    signOut(auth).then(() => {
-      navigate('/signin')
-    }).catch((error) => {
-        console.log(error.message)
-    });
-  }
-  const addedPost = (brand, model, color, price, moneyType, mileage, distanceType, horsepower, body, gearbox, handDrive, engine, additionalInfo) => {
+    signOut(auth)
+      .then(() => {
+        navigate("/signin");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const addedPost = (
+    brand,
+    model,
+    color,
+    price,
+    moneyType,
+    mileage,
+    distanceType,
+    horsepower,
+    body,
+    gearbox,
+    handDrive,
+    engine,
+    additionalInfo
+  ) => {
     return dispatch({
-      type: 'added-user-post',
+      type: "added-user-post",
       payload: {
         brand: brand,
         model: model,
@@ -77,18 +94,30 @@ function App() {
         gearbox: gearbox,
         handDrive: handDrive,
         engine: engine,
-        additionalInfo: additionalInfo
-      }
-    })
-  }
+        additionalInfo: additionalInfo,
+      },
+    });
+  };
   return (
     <>
-    <Nav logout={logout} />
+      <Nav logout={logout} />
       <Routes>
-          <Route path='/*' element={<Home />} > Home </Route>
-          <Route path='signin'  element={<SignIn signIn={signIn} />}>  Sign In</Route> 
-          <Route path='signup' element={<SignUp username={user} signUp={signUp} /> }> Sign Up</Route>
-        </Routes>
+        <Route path="/*" element={<Home />}>
+          {" "}
+          Home{" "}
+        </Route>
+        <Route path="signin" element={<SignIn signIn={signIn} />}>
+          {" "}
+          Sign In
+        </Route>
+        <Route
+          path="signup"
+          element={<SignUp username={user} signUp={signUp} />}
+        >
+          {" "}
+          Sign Up
+        </Route>
+      </Routes>
     </>
   );
 }
